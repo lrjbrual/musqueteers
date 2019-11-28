f65bb77d65f3c89b862013a77e64cfeea4029d8e
Ryan Jeff Brual <lrjbrual@live.com>
2019-11-28 11:33:24 +0100

    update creation of pdf via wickedpdf

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_11_28_095111) do
+
+  create_table "invoice_items", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.decimal "price"
+    t.integer "qty"
+    t.integer "invoice_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.string "from_full_name"
+    t.string "from_address"
+    t.string "from_email"
+    t.string "from_phone"
+    t.string "to_full_name"
+    t.string "to_address"
+    t.string "to_email"
+    t.string "to_phone"
+    t.string "status"
+    t.decimal "discount"
+    t.decimal "vat"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "invoice_items", "invoices"
+end
